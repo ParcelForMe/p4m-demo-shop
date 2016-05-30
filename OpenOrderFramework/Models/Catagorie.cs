7fabce56834106befe5cbda255394775f83eb635
Barry Earsman <barry@earsman.com>
2016-05-30 13:22:18 +1000

    Initial code commit

---
+++ OpenOrderFramework/Models/Catagorie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace OpenOrderFramework.Models
+{
+    public class Catagorie
+    {
+        [Key]
+        [DisplayName("Catagory ID")]
+        public int ID { get; set; }
+
+        [DisplayName("Catagory")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Item> Items { get; set; }
+    }
+}
