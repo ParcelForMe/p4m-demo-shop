b2b0625f4538e04f39ad9a746bcfa1efb3e6553a
Michael Strong <mstrong64@gmail.com>
2016-06-13 16:54:32 +1000

    First hack at adding in tax, shipping and discounts Add P4M widget endpoints

---
+++ OpenOrderFramework/Models/P4MMessages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenOrderFramework.Models
+{
+    public class P4MBaseMessage
+    {
+        public bool Success { get { return string.IsNullOrWhiteSpace(Error); } }
+        public string Error { get; set; }
+    }
+
+    public class DiscountMessage : P4MBaseMessage
+    {
+        public decimal Tax { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    public class CartUpdateMessage : P4MBaseMessage
+    {
+        public decimal Tax { get; set; }
+        public decimal Shipping { get; set; }
+    }
+}
