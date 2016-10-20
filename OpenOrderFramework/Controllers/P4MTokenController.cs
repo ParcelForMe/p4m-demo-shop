56a35c77c55b7cd1e6ea7095cbb2cb9ed5298a2e
Barry Earsman <barry@earsman.com>
2016-10-20 12:33:19 +1000

    Address preselection based on preferences

--- OpenOrderFramework/Controllers/P4MTokenController.cs
+++ OpenOrderFramework/Controllers/P4MTokenController.cs
@@ -35,10 +35,12 @@ namespace OpenOrderFramework.Controllers
         public const string ClientGuestId = "codeclient_guest";
 
         public const string BaseAddress = "https://local.parcelfor.me:44333/core";
-//        public const string BaseAddress = "https://local.parcelfor.me:44333";
-        public const string BaseApiAddress = "https://local.parcelfor.me:44321/api/v2/";
+        //public const string BaseAddress = "https://local.parcelfor.me:44333";
         //public const string BaseAddress = "https://dev.parcelfor.me:44333/core";
+
+        public const string BaseApiAddress = "https://local.parcelfor.me:44321/api/v2/";
         //public const string BaseApiAddress = "https://dev.parcelfor.me:44321/api/v2/";
+
         public const string BaseIdSrvAddress = BaseAddress + "/ui/";
         public const string LocalCallbackUrl = "http://localhost:3000/p4m/getP4MAccessToken";
 
